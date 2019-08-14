388bc32e159117f4b3b058a9289929e5f6126ba3
jackeybiao <hanbiao4079@qq.com>
2019-08-14 14:29:26 +0800

    新增前端页面

--- wwwroot/src/pages/admin/AdminIndex.js
+++ wwwroot/src/pages/admin/AdminIndex.js
@@ -1,9 +1,55 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+
+import './admin.css';
 
 export function AdminIndex(){
+  const list = [
+    {
+      id:"1",
+      title:"标题1",
+      content:"内容1",
+      time:'2019-1-1',
+      author:"jackeybiao",
+      comment:[],
+    },
+    {
+      id:"2",
+      title:"标题2",
+      content:"内容2",
+      time:'2019-1-2',
+      author:"jackeybiao",
+      comment:[],
+    },
+    {
+      id:"3",
+      title:"标题3",
+      content:"内容3",
+      time:'2019-1-3',
+      author:"jackeybiao",
+      comment:[],
+    },
+  ];
   return (
-    <div>
-      index
+    <div className="index">
+      <div className="index-title">
+        <button><Link to="/post/">新增</Link></button>
+      </div>
+      {list.map(item=>{
+        return (
+          <div className="index-item" key={item.id}>
+            <Link to={"/post/" + item.id}>
+              <div className="title">{item.title}</div>
+              <div className="author">{item.author}</div>
+              <div className="content">{item.content}</div>
+            </Link>
+            <div className="btn-box">
+              <button><Link to={"/post/" + item.id}>编辑</Link></button>
+              <button>删除</button>
+            </div>
+          </div>
+        )
+      })}
     </div>
   )
 }
--- wwwroot/src/pages/admin/AdminPost.js
+++ wwwroot/src/pages/admin/AdminPost.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
+import './admin.css';
+
 export function AdminPost(){
   return (
-    <div>
-      <ul>
-        <li></li>
-      </ul>
+    <div className="post-content">
+      <form>
+        <div className="form-group">
+          <label htmlFor="">标题</label>
+          <input type="text"/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="">正文</label>
+          <textarea name="" id="content" cols="30" rows="10"></textarea>
+        </div>
+        <div className="form-group">
+          <button>保存</button>
+        </div>
+      </form>
     </div>
   )
 }
